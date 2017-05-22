ad88ebc54d8049f9eaa182c0e3ccfbe0e982e4d5
jairos1301 <jairos1301@gmail.com>
2017-05-22 09:41:10 -0500

    Mensaje
    Se agrego el mensaje de inicio

--- src/parciallaboratorio/ParcialLaboratorio.java
+++ src/parciallaboratorio/ParcialLaboratorio.java
@@ -17,7 +17,7 @@ public class ParcialLaboratorio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("PERMITE DIVIDIR DOS NUMEROS");
         int num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 1"));
         int num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 2"));
         int div = num1/num2;
